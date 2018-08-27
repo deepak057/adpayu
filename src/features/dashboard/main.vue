dcab116a79b96c321ccb733e65504af0585fe12f
mansa-dev <developer@mansainfotech.com>
2018-08-27 16:03:58 +0530

    Implemented comments and Likes features

--- src/features/dashboard/main.vue
+++ src/features/dashboard/main.vue
@@ -57,16 +57,40 @@
                     span.badge.badge-info.ml-auto.f-w-400.pr-t--2.f-s-12.bg-999.cursor-hand(data-container="body" title="Ad Revenue" data-toggle="popover" data-placement="right" :data-content="getCPCText(f['cpc'])") + $ {{f['cpc']}}
                       i.mdi.mdi-information.m-l-4
                   .like-comm
-                    a.link.m-r-10(href='javascript:void(0)') 2 comment
-                    a.link.m-r-10(href='javascript:void(0)')
-                      i.fa.fa-heart.text-danger
-                      |  5 Love
+                    a.link.m-r-10(href='javascript:void(0)' @click="toggleComments(f)") {{f['comments'].length}} {{f['comments'].length>1? "Comments": 'comment'}}
+                    a.link.m-r-10(href='javascript:void(0)' @click="loveToggle(f)" title="Click to like or unlike it")
+                      i.text-danger(:class="{'ti-heart pr-t-2': !f['love'].loved, 'fa fa-heart': f['love'].loved}")
+                      |  {{f['love'].total}} {{f['love'].loved? 'Loved': 'Love'}}
+              .comment-widgets(v-show="f['showComments']")
+                .d-flex.flex-row.comment-row(v-for="(comment, n) in f['comments']")
+                  .p-2
+                    span.round
+                      img(src='static/assets/images/users/1.jpg', alt='user', width='50')
+                  .comment-text.w-100
+                    h5 James Anderson
+                    p.m-b-5
+                      | {{comment.comment}}
+                    .comment-footer
+                      span.text-muted.pull-right {{comment.date}}
+                      span.action-icons
+                        a(href='javascript:void(0)')
+                          i.ti-pencil-alt
+                        a(href='javascript:void(0)' @click='deleteComment(f, n)')
+                          i.ti-trash
+                        a(href='javascript:void(0)')
+                          i.ti-heart
+                .row.m-t-10
+                  .col-11
+                    textarea.form-control.b-0(placeholder='Type your message here' v-model.trim="newCommentText" @keyup.enter="leaveComment(f)")
+                  .col-1.text-right
+                    button.btn.btn-info.btn-circle.btn-lg(type='button' @click='leaveComment(f)')
+                      i.fa.fa-paper-plane-o.pr-t--3-l--3
               hr
           .nothing-to-show(v-show="!newsFeedEnabled && !adEnabled")
             .jumbotron.white-back.text-center
               h1.display-3 Dead End!
               p.lead
-                | You need to enable either News Feed or ads to see something here.
+                | You need to enable either News Feed or Ads to see something here.
               hr.my-4
               p
                 | Currently, you have disabled both.
@@ -153,6 +177,7 @@ export default {
     return {
       adEnabled: true,
       newsFeedEnabled: true,
+      newCommentText: '',
       feed: [{
         type: 'ad',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper',
@@ -165,7 +190,24 @@ export default {
         cpv: 0.2,
         cpc: 1,
         video: false,
-        show: true
+        show: true,
+        love: {
+          total: 2,
+          loved: true
+        },
+        showComments: false,
+        comments: [
+          {
+            uid: 2,
+            comment: 'Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has beenorem Ipsum is simply dummy text of the printing and type setting industry.',
+            date: 'April 14, 2018'
+          },
+          {
+            uid: 1,
+            comment: 'Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has beenorem Ipsum is simply dummy text of the printing and type setting industry.',
+            date: 'April 12, 2018'
+          }
+        ]
       },
       {
         type: 'newsFeed',
@@ -176,7 +218,13 @@ export default {
         cpv: 0.2,
         cpc: 0,
         video: false,
-        show: true
+        show: true,
+        love: {
+          total: 5,
+          loved: false
+        },
+        showComments: false,
+        comments: []
       },
       {
         type: 'ad',
@@ -197,7 +245,19 @@ export default {
             poster: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
           }
         },
-        show: true
+        show: true,
+        love: {
+          total: 2,
+          loved: false
+        },
+        showComments: false,
+        comments: [
+          {
+            uid: 4,
+            comment: 'Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has beenorem Ipsum is simply dummy text of the printing and type setting industry.',
+            date: 'April 14, 2018'
+          }
+        ]
       }
       ]
     }
@@ -233,6 +293,26 @@ export default {
           this.feed[i]['show'] = this.newsFeedEnabled
         }
       }
+    },
+    loveToggle (feedItem) {
+      if (feedItem['love'].loved) {
+        feedItem['love'].total--
+      } else {
+        feedItem['love'].total++
+      }
+      feedItem['love'].loved = !feedItem['love'].loved
+    },
+    toggleComments (feedItem) {
+      feedItem['showComments'] = !feedItem['showComments']
+    },
+    leaveComment (feedItem) {
+      if (this.newCommentText) {
+        this.$set(feedItem['comments'], feedItem['comments'].length, {uid: 1, comment: this.newCommentText, date: '21 August, 2018'})
+        this.newCommentText = ''
+      }
+    },
+    deleteComment (feedItem, index_) {
+      feedItem['comments'].splice(index_, 1)
     }
   }
 }
