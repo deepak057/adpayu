3273544bd9e3b7fe5b792ad1db54067769976aad
mansa <deepak057@yahoo.com>
2019-01-25 12:34:28 +0530

    redesigned the dearch bar in top nav

--- src/components/app-bar.vue
+++ src/components/app-bar.vue
@@ -43,94 +43,6 @@ header.topbar
           // ==============================================================
           // Messages
           // ==============================================================
-          li.nav-item.dropdown.mega-dropdown
-            a.nav-link.dropdown-toggle.text-muted.waves-effect.waves-dark(href='', data-toggle='dropdown', aria-haspopup='true', aria-expanded='false')
-              i.mdi.mdi-view-grid
-            .dropdown-menu.scale-up-left
-              ul.mega-dropdown-menu.row
-                li.col-lg-3.col-xlg-2.m-b-30
-                  h4.m-b-20 CAROUSEL
-                  // CAROUSEL
-                  #carouselExampleControls.carousel.slide(data-ride='carousel')
-                    .carousel-inner(role='listbox')
-                      .carousel-item.active
-                        .container
-                          img.d-block.img-fluid(src='static/assets/images/big/img1.jpg', alt='First slide')
-                      .carousel-item
-                        .container
-                          img.d-block.img-fluid(src='static/assets/images/big/img2.jpg', alt='Second slide')
-                      .carousel-item
-                        .container
-                          img.d-block.img-fluid(src='static/assets/images/big/img3.jpg', alt='Third slide')
-                    a.carousel-control-prev(href='#carouselExampleControls', role='button', data-slide='prev')
-                      span.carousel-control-prev-icon(aria-hidden='true')
-                      span.sr-only Previous
-                    a.carousel-control-next(href='#carouselExampleControls', role='button', data-slide='next')
-                      span.carousel-control-next-icon(aria-hidden='true')
-                      span.sr-only Next
-                  // End CAROUSEL
-                li.col-lg-3.m-b-30
-                  h4.m-b-20 ACCORDION
-                  // Accordian
-                  #accordion.nav-accordion(role='tablist', aria-multiselectable='true')
-                    .card
-                      #headingOne.card-header(role='tab')
-                        h5.mb-0
-                          a(data-toggle='collapse', data-parent='#accordion', href='#collapseOne', aria-expanded='true', aria-controls='collapseOne')
-                            | Collapsible Group Item #1
-                      #collapseOne.collapse.show(role='tabpanel', aria-labelledby='headingOne')
-                        .card-body  Anim pariatur cliche reprehenderit, enim eiusmod high.
-                    .card
-                      #headingTwo.card-header(role='tab')
-                        h5.mb-0
-                          a.collapsed(data-toggle='collapse', data-parent='#accordion', href='#collapseTwo', aria-expanded='false', aria-controls='collapseTwo')
-                            | Collapsible Group Item #2
-                      #collapseTwo.collapse(role='tabpanel', aria-labelledby='headingTwo')
-                        .card-body
-                          | Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                    .card
-                      #headingThree.card-header(role='tab')
-                        h5.mb-0
-                          a.collapsed(data-toggle='collapse', data-parent='#accordion', href='#collapseThree', aria-expanded='false', aria-controls='collapseThree')
-                            | Collapsible Group Item #3
-                      #collapseThree.collapse(role='tabpanel', aria-labelledby='headingThree')
-                        .card-body
-                          | Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
-                li.col-lg-3.m-b-30
-                  h4.m-b-20 CONTACT US
-                  // Contact
-                  form
-                    .form-group
-                      input#exampleInputname1.form-control(placeholder='Enter Name', type='text')
-                    .form-group
-                      input.form-control(placeholder='Enter email', type='email')
-                    .form-group
-                      textarea#exampleTextarea.form-control(rows='3', placeholder='Message')
-                    button.btn.btn-info(type='submit') Submit
-                li.col-lg-3.col-xlg-4.m-b-30
-                  h4.m-b-20 List style
-                  // List style
-                  ul.list-style-none
-                    li
-                      a(href='javascript:void(0)')
-                        i.fa.fa-check.text-success
-                        |  You can give link
-                    li
-                      a(href='javascript:void(0)')
-                        i.fa.fa-check.text-success
-                        |  Give link
-                    li
-                      a(href='javascript:void(0)')
-                        i.fa.fa-check.text-success
-                        |  Another Give link
-                    li
-                      a(href='javascript:void(0)')
-                        i.fa.fa-check.text-success
-                        |  Forth link
-                    li
-                      a(href='javascript:void(0)')
-                        i.fa.fa-check.text-success
-                        |  Another fifth link
           // ==============================================================
           // End Messages
           // ==============================================================
@@ -142,63 +54,6 @@ header.topbar
           // Comment
           // ==============================================================
           <notification-dropdown></notification-dropdown>
-          // ==============================================================
-          // Messages
-          // ==============================================================
-          li.nav-item.dropdown
-            a#2.nav-link.dropdown-toggle.text-muted.waves-effect.waves-dark(href='', data-toggle='dropdown', aria-haspopup='true', aria-expanded='false')
-              i.mdi.mdi-email
-              .notify
-                span.heartbit
-                span.point
-            .dropdown-menu.mailbox.dropdown-menu-right.scale-up(aria-labelledby='2')
-              ul
-                li
-                  .drop-title You have 4 new messages
-                li
-                  .message-center
-                    // Message
-                    a(href='#')
-                      .user-img
-                        img.img-circle(src='static/assets/images/users/1.jpg', alt='user')
-                        span.profile-status.online.pull-right
-                      .mail-contnet
-                        h5 Pavan kumar
-                        span.mail-desc Just see the my admin!
-                        span.time 9:30 AM
-                    // Message
-                    a(href='#')
-                      .user-img
-                        img.img-circle(src='static/assets/images/users/2.jpg', alt='user')
-                        span.profile-status.busy.pull-right
-                      .mail-contnet
-                        h5 Sonu Nigam
-                        span.mail-desc I've sung a song! See you at
-                        span.time 9:10 AM
-                    // Message
-                    a(href='#')
-                      .user-img
-                        img.img-circle(src='static/assets/images/users/3.jpg', alt='user')
-                        span.profile-status.away.pull-right
-                      .mail-contnet
-                        h5 Arijit Sinh
-                        span.mail-desc I am a singer!
-                        span.time 9:08 AM
-                    // Message
-                    a(href='#')
-                      .user-img
-                        img.img-circle(src='static/assets/images/users/4.jpg', alt='user')
-                        span.profile-status.offline.pull-right
-                      .mail-contnet
-                        h5 Pavan kumar
-                        span.mail-desc Just see the my admin!
-                        span.time 9:02 AM
-                li
-                  a.nav-link.text-center(href='javascript:void(0);')
-                    strong See all e-Mails
-                    i.fa.fa-angle-right
-          // ==============================================================
-          // End Messages
           // ==============================================================
           // ==============================================================
           // Profile
@@ -242,25 +97,6 @@ header.topbar
                   a(href='javascript:void(0)' @click="logout")
                     i.fa.fa-power-off
                     |  Logout
-          // ==============================================================
-          // Language
-          // ==============================================================
-          li.nav-item.dropdown
-            a.nav-link.dropdown-toggle.text-muted.waves-effect.waves-dark(href='', data-toggle='dropdown', aria-haspopup='true', aria-expanded='false')
-              i.flag-icon.flag-icon-us
-            .dropdown-menu.dropdown-menu-right.scale-up
-              a.dropdown-item(href='#')
-                i.flag-icon.flag-icon-in
-                |  India
-              a.dropdown-item(href='#')
-                i.flag-icon.flag-icon-fr
-                |  French
-              a.dropdown-item(href='#')
-                i.flag-icon.flag-icon-cn
-                |  China
-              a.dropdown-item(href='#')
-                i.flag-icon.flag-icon-de
-                |  Dutch
 </template>
 
 <script>
--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
-li.nav-item.hidden-sm-down.search-box
-  a.nav-link.hidden-sm-down.text-muted.waves-effect.waves-dark(href='javascript:void(0)' :id="closeButtonId")
-    i.ti-search
-  form.app-search(@submit.prevent="onSubmit")
-    input.form-control(placeholder='Search & enter', type='text' v-model="k")
-    a.srh-btn
-      i.ti-close
+li.nav-item.search-box
+  form(@submit.prevent="onSubmit")
+    input.nav-search-field.form-control(placeholder="Search..." type='text' v-model="k")
+  // a.nav-link.text-muted.waves-effect.waves-dark(href='javascript:void(0)' :id="closeButtonId")
+    // i.ti-search
+  // form.app-search(@submit.prevent="onSubmit")
+    // input.form-control(placeholder='Search & enter', type='text' v-model="k")
+    // a.srh-btn
+      // i.ti-close
 </template>
 <script>
 import { router } from '@/http'
@@ -20,7 +22,7 @@ export default {
   },
   methods: {
     onSubmit () {
-      this.closeSearchBar()
+      // this.closeSearchBar()
       router.push({
         name: 'search',
         params: {
@@ -30,7 +32,7 @@ export default {
           k: this.k
         }
       })
-      // this.k = ''
+      this.k = ''
     },
     closeSearchBar () {
       document.getElementById(this.closeButtonId).click()
