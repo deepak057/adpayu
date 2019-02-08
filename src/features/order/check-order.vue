b789919eed3b54b3efd1cfcbf1bcc880a5bf02f8
mansa <deepak057@yahoo.com>
2019-02-08 13:03:10 +0530

    working on the creating post after payment

--- src/features/order/check-order.vue
+++ src/features/order/check-order.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
 div
+  .text-danger.m-t-20(v-if="error")
+    strong
+      i.mdi.mdi-alert
+      |  {{error}}
   .page-preloader(v-if="pageLoader")
     <preloader/>
     br
     .m-t-20
-      h5 Checking Payment Status...
+      h5.f-w-500 Checking Payment Status...
+      p Please don't leave the page
 </template>
 <script>
 // import auth from '@/auth/helpers'
@@ -21,7 +26,33 @@ export default {
   mixins: [mixin],
   data () {
     return {
-      pageLoader: true
+      pageLoader: true,
+      error: false
+    }
+  },
+  mounted () {
+    this.init()
+  },
+  methods: {
+    init () {
+      let orderId = this.getOrderId()
+      if (orderId) {
+        this.$options.service.checkPaymentStatus(orderId)
+          .then((data) => {
+          })
+          .catch((pErr) => {
+            alert('Soemthing went wrong while checking the payment status. Please contact us if you need more details.')
+          })
+      }
+    },
+    getOrderId () {
+      if (this.$route.query.orderId) {
+        return this.$route.query.orderId
+      } else {
+        this.error = 'Error: Soemthing went wrong. The link might be broken or incorrect. Please contact us if you need more details.'
+        this.pageLoader = false
+        return false
+      }
     }
   }
 }
--- src/features/order/service.js
+++ src/features/order/service.js
@@ -15,4 +15,13 @@ export default class Service {
         return new Promise((resolve, reject) => { reject(error) })
       })
   }
+  checkPaymentStatus (orderId) {
+    return auth.get('/payment/checkOrderStatus/' + orderId)
+      .then((response) => {
+        return new Promise((resolve) => { resolve(response) })
+      })
+      .catch((error) => {
+        return new Promise((resolve, reject) => { reject(error) })
+      })
+  }
 }
