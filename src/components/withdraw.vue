44f2689bc8899bafb5fa893d753ccb692f65c9eb
mansa <deepak057@yahoo.com>
2019-03-12 22:48:26 +0530

    working on payment withdrawl

--- src/components/withdraw.vue
+++ src/components/withdraw.vue
@@ -10,6 +10,7 @@ div
         .modal-body
           .text-center.m-t-20(v-if="pageLoader")
             <preloader/>
+          <template v-if="!data.overviewGone">
           .text-center(v-if="!pageLoader")
             .table-responsive
               table.table.table-hover
@@ -21,27 +22,64 @@ div
                   tr
                     td
                       | Money Accumulated
-                      i.mdi.mdi-information-outline.m-l-5.cursor-hand
+                      i.mdi.mdi-information-outline.m-l-5.cursor-hand(data-container="body" title="Money Accumulated" data-toggle="popover" data-placement="right" data-content="Total amount of money that you have made by seeing and watching the ads.")
                     td ${{data.transactionDetails.amountAccumulatedUSD}}
                     td {{data.transactionDetails.amountAccumulatedINR}} INR
                   tr
                     td
                       | {{data.transactionDetails.siteFeePercentage}}% Site Fee
-                      i.mdi.mdi-information-outline.m-l-5.cursor-hand
+                      i.mdi.mdi-information-outline.m-l-5.cursor-hand(data-container="body" title="Site Fee" data-toggle="popover" data-placement="right" :data-content="getSiteFeeText()")
                     td.text-danger ${{data.transactionDetails.siteFeeUSD}}
                     td.text-danger {{data.transactionDetails.siteFeeINR}} INR
                   tr
                     td
                       h4
                         | Total Payable
-                        i.mdi.mdi-information-outline.m-l-5.cursor-hand
+                        i.mdi.mdi-information-outline.m-l-5.cursor-hand(data-container="body" title="Total Payable Money" data-toggle="popover" data-placement="right" data-content="This is the amount of money that is payable to you after deducting the Site Fee from the total accumulated amount.")
                     td
                       h4.text-success ${{data.transactionDetails.totalUSD}}
                     td
                       h4.text-success {{data.transactionDetails.totalINR}} INR
+          </template>
+          <template v-if="data.overviewGone">
+          .payment-withdrawl-options-wrap(v-if="!pageLoader")
+            h4 Transfer money to your?
+            .m-t-20
+              input.m-r-5.with-gap(v-model="data.transfermodeDetails.mode" name="group4" value="bank" type="radio" id="pwo-rd-bank-account")
+              label.m-r-5(for="pwo-rd-bank-account")
+                | Bank Account
+              input.m-r-5.with-gap(v-model="data.transfermodeDetails.mode" name="group4" type="radio" value="paytm" id="pwo-rd-paytm")
+              label(for="pwo-rd-paytm")
+                | Paytm
+              .pwo-selected-option-wrap.m-t-20
+                <template v-if="data.transfermodeDetails.mode === 'bank'">
+                  form
+                    .form-group
+                      label Account Number*
+                      input.form-control(type="text" placeholder="Enter Bank Account Number")
+                    .form-group
+                      label IFSC*
+                      input.form-control(type="text" placeholder="Enter IFSC code")
+                    .form-group
+                      label Phone Number*
+                      input.form-control(type="text" placeholder="Enter Your Phone Number")
+                    .form-group
+                      label Address*
+                      input.form-control(type="text" placeholder="Enter Your Address")
+                </template>
+                <template v-if="data.transfermodeDetails.mode === 'paytm'">
+                  form
+                    .form-group
+                      label Paytm mobile number*
+                      input.form-control(type="text" placeholder="Registered Paytm mobile number")
+                    .form-group
+                      label Address*
+                      input.form-control(type="text" placeholder="Enter Your Address")
+                </template>
+          </template>
         .modal-footer
           button.btn.btn-default.waves-effect(type='button', data-dismiss='modal' :id="closeButtonId") Close
-          button.btn.btn-danger.waves-effect(type='button', data-dismiss='modal') Proceed
+          button.btn.btn-danger.waves-effect(type='button', @click="proceedToPaymentOptionScreen()") Proceed
 </template>
 <script>
 import mixin from '../globals/mixin'
@@ -51,7 +89,13 @@ import Service from './service'
 
 function getWithdrawInitialState () {
   return {
-    transactionDetails: false
+    transactionDetails: false,
+    overviewGone: false,
+    transfermodeDetails: {
+      mode: 'bank',
+      accountNumber: 2121111,
+      IFSC: 'Ne3'
+    }
   }
 }
 
@@ -109,6 +153,13 @@ export default {
     },
     closePopup () {
       document.getElementById(this.closeButtonId).click()
+    },
+    getSiteFeeText () {
+      return 'We will keep ' + this.data.transactionDetails.siteFeePercentage + '% of the money you have made as part of our service fee. This money will help us continue to run and suport this platform.'
+    },
+    proceedToPaymentOptionScreen () {
+      this.data.overviewGone = true
+      // this.pageLoader = true
     }
   }
 }
