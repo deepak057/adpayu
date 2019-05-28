baaf19d6965b5fe4b4d57d38ff555a5a24c8ac3a
mansa <deepak057@yahoo.com>
2019-05-28 14:47:02 +0530

    working on feed changes

--- src/components/feed/feed.vue
+++ src/components/feed/feed.vue
@@ -88,7 +88,7 @@
               a.dropdown-item(href='javascript:void(0)' @click="deletePost(f, k)")
                 i.mdi.mdi-delete.m-r-5
                 | Delete
-    <comments @CommentsCountUpdated = "updateCommentsCount" :commentType="f['type']" :postId="f['id']" v-if="f['showComments']" @closeModal="leavePage"></comments>
+    <comments @CommentsCountUpdated = "updateCommentsCount" :commentType="f['type']" :postId="f['id']" v-if="f['showComments']" :class="{'question-on-user-feed': userFeed && f['Question']}" @closeModal="leavePage"></comments>
     hr
   <ad-stats ref="adStatsComponent"/>
   <edit-post ref="editPostComponent" @PostUpdated="updatePost"/>
