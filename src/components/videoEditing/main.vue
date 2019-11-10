86732a50e75aa75d50401b9e3bddbf36150139de
mansa <deepak057@yahoo.com>
2019-11-10 13:55:03 +0530

    working on video edtiign

--- src/components/videoEditing/main.vue
+++ src/components/videoEditing/main.vue
@@ -9,64 +9,117 @@ div(v-if="triggered")
             | Edit Video
           button.close(type='button', data-dismiss='modal', aria-hidden='true') ×
         .modal-body
-          #accordionExample.accordion.accordion-blue
+          .accordion.accordion-blue(:id="getSectionId(0)")
             .card
-              #headingOne.card-header(data-toggle='collapse', data-target='#collapseOne', aria-expanded='true', aria-controls='collapseOne')
+              .card-header(:id="getSectionId(1, 'header')" data-toggle='collapse', :data-target="'#'+getSectionId(1)", aria-expanded='true', :aria-controls='getSectionId(1)')
                 h2.mb-0
                   button.btn(type='button')
                     i.fa.fa-music.m-r-10
                     span.tab-label
                       | Background Music
-              #collapseOne.collapse(aria-labelledby='headingOne', data-parent='#accordionExample')
+              .collapse(:id="getSectionId(1)" :aria-labelledby="getSectionId(1, header)", :data-parent="'#'+getSectionId(0)")
                 .card-body
-                  | Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                  .row
+                    .col-lg-4.col-md-6
+                      .card.m-b-30
+                        .card-body
+                          .row
+                            .col-4
+                              .pointer.round.round-lg.align-self-center.round-info
+                                i.mdi.mdi.mdi-play
+                            .col-8.p-0
+                              .m-l-10.align-self-center
+                                h4.m-b-0.font-light
+                                  a.c-inherit(href='/tag/general') General
+                                h5.m-t-5.text-muted.m-b-0
+                                  span
+                                    span.pointer Add
+                                    .btn-group(style='display: none;')
+                                      button.btn.btn-secondary.dropdown-toggle(type='button', data-toggle='dropdown', aria-haspopup='true', aria-expanded='false')
+                                        i.fa.fa-check.m-r-5
+                                        span Following
+                                      .dropdown-menu
+                                        a.dropdown-item(href='javascript:void(0)') Unfollow
+                    .col-lg-4.col-md-6
+                      .card.m-b-30
+                        .card-body
+                          .row
+                            .col-4
+                              .pointer.round.round-lg.align-self-center.round-info
+                                i.mdi.mdi.mdi-play
+                            .col-8.p-0
+                              .m-l-10.align-self-center
+                                h4.m-b-0.font-light
+                                  a.c-inherit(href='/tag/general') General
+                                h5.m-t-5.text-muted.m-b-0
+                                  span
+                                    span.pointer Add
+                                    .btn-group(style='display: none;')
+                                      button.btn.btn-secondary.dropdown-toggle(type='button', data-toggle='dropdown', aria-haspopup='true', aria-expanded='false')
+                                        i.fa.fa-check.m-r-5
+                                        span Following
+                                      .dropdown-menu
+                                        a.dropdown-item(href='javascript:void(0)') Unfollow
+                    .col-lg-4.col-md-6
+                      .card.m-b-30
+                        .card-body
+                          .row
+                            .col-4
+                              .pointer.round.round-lg.align-self-center.round-info
+                                i.mdi.mdi.mdi-play
+                            .col-8.p-0
+                              .m-l-10.align-self-center
+                                h4.m-b-0.font-light
+                                  a.c-inherit(href='/tag/general') General
+                                h5.m-t-5.text-muted.m-b-0
+                                  span
+                                    span.pointer Add
+                                    .btn-group(style='display: none;')
+                                      button.btn.btn-secondary.dropdown-toggle(type='button', data-toggle='dropdown', aria-haspopup='true', aria-expanded='false')
+                                        i.fa.fa-check.m-r-5
+                                        span Following
+                                      .dropdown-menu
+                                        a.dropdown-item(href='javascript:void(0)') Unfollow
+                    .col-lg-4.col-md-6
+                      .card.m-b-30
+                        .card-body
+                          .row
+                            .col-4
+                              .pointer.round.round-lg.align-self-center.round-info
+                                i.mdi.mdi.mdi-play
+                            .col-8.p-0
+                              .m-l-10.align-self-center
+                                h4.m-b-0.font-light
+                                  a.c-inherit(href='/tag/general') General
+                                h5.m-t-5.text-muted.m-b-0
+                                  span
+                                    span.pointer Add
+                                    .btn-group(style='display: none;')
+                                      button.btn.btn-secondary.dropdown-toggle(type='button', data-toggle='dropdown', aria-haspopup='true', aria-expanded='false')
+                                        i.fa.fa-check.m-r-5
+                                        span Following
+                                      .dropdown-menu
+                                        a.dropdown-item(href='javascript:void(0)') Unfollow
             .card
-              #headingTwo.card-header(data-toggle='collapse', data-target='#collapseTwo', aria-expanded='true', aria-controls='collapseTwo')
+              .card-header(:id="getSectionId(2, 'header')" data-toggle='collapse', :data-target="'#'+getSectionId(2)", aria-expanded='true', :aria-controls='getSectionId(2)')
                 h2.mb-0
                   button.btn(type='button')
                     i.fa.fa-cut.m-r-10
                     span.tab-label
                       | Trim Video
-              #collapseTwo.collapse(aria-labelledby='headingTwo', data-parent='#accordionExample')
+              .collapse(:id="getSectionId(2)" :aria-labelledby="getSectionId(2, header)", :data-parent="'#'+getSectionId(0)")
                 .card-body
                   | Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
             .card
-              #headingThree.card-header(data-toggle='collapse', data-target='#collapseThree', aria-expanded='true', aria-controls='collapseThree')
+              .card-header(:id="getSectionId(3, 'header')" data-toggle='collapse', :data-target="'#'+getSectionId(3)", aria-expanded='true', :aria-controls='getSectionId(3)')
                 h2.mb-0
                   button.btn(type='button')
                     i.fa.fa-image.m-r-10
                     span.tab-label
                       | Video Screenshot
-              #collapseThree.collapse(aria-labelledby='headingThree', data-parent='#accordionExample')
+              .collapse(:id="getSectionId(3)" :aria-labelledby="getSectionId(3, header)", :data-parent="'#'+getSectionId(0)")
                 .card-body
                   | Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-
-          //div
-            ul.nav.nav-tabs.customtab(role='tablist')
-              li.nav-item
-                a.nav-link.active(data-toggle='tab', :href="'#' + getTabId(1)", role='tab' , aria-expanded='true')
-                  // span.hidden-sm-up
-                    i.ti-home
-                  span Background Music
-              li.nav-item
-                a.nav-link(data-toggle='tab', :href="'#' + getTabId(2)", role='tab' , aria-expanded='false')
-                  // span.hidden-sm-up
-                    i.ti-user
-                  span Trim Video
-              li.nav-item
-                a.nav-link(data-toggle='tab', :href="'#' + getTabId(3)", role='tab' , aria-expanded='false')
-                  // span.hidden-sm-up
-                    i.ti-email
-                  span Video Screenshot
-            .tab-content
-              .tab-pane.active(:id="getTabId(1)" role='tabpanel', aria-expanded='false')
-                .p-20
-                  h3 Best Clean Tab ever
-                  h4 you can use it with the small code
-                  p
-                    | Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a.
-              .tab-pane.p-20(:id="getTabId(2)" role='tabpanel', aria-expanded='false') 2
-              .tab-pane.p-20(:id="getTabId(3)" role='tabpanel', aria-expanded='false') 3
         .modal-footer
           button.btn.btn-default.waves-effect(type='button', data-dismiss='modal' :id="closeButtonId") Close
           button.btn.btn-danger.waves-effect.waves-light(@click="proceedToPay()")
@@ -118,8 +171,8 @@ export default {
     closePopup () {
       document.getElementById(this.closeButtonId).click()
     },
-    getTabId (index) {
-      return this.id + '-tab-' + index
+    getSectionId (index, type_ = 'collapse') {
+      return this.id + '-' + type_ + '-' + index
     },
     triggerPopup () {
       /*eslint-disable*/
