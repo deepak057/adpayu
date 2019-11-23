a4d903c436d39d6acdf9fcfbdb2e31d3c8f8a393
mansa <deepak057@yahoo.com>
2019-11-23 12:40:31 +0530

    working on background music editing

--- src/components/videoEditing/add_music.vue
+++ src/components/videoEditing/add_music.vue
@@ -2,7 +2,7 @@
 div(v-if="triggered")
   span(:id="triggerButtonId" data-toggle="modal" :data-target="modalIdHash" data-keyboard="false")
   .modal.modal-append-to-body.topmost-modal(:id="modalId" tabindex='-1', role='dialog', aria-label.smallledby='AdStatsModallabel.small', aria-hidden='true')
-    .modal-dialog.modal-lg
+    .modal-dialog
       .modal-content
         .modal-header
           h4.modal-title
@@ -10,6 +10,20 @@ div(v-if="triggered")
           button.close(type='button', data-dismiss='modal', aria-hidden='true') ×
         .modal-body
           div
+            form
+              .form-group
+                label Track name/title*
+                input.form-control(type="text" placeholder="Enter here")
+              .form-group
+                label Genere*
+                select.form-control.custom-select
+                  option(value='') Genere (All)
+                  option(value='') Female
+              .form-group
+                label
+                  i.mdi.mdi-upload.m-r-5.v-align-top.f-s-20
+                  | Upload Audio File
+              file.none(:id="fileElementId")
         .modal-footer
           button.btn.btn-default.waves-effect(type='button', data-dismiss='modal' :id="closeButtonId") Cancel
           button.btn.btn-danger.waves-effect.waves-light
@@ -43,6 +57,11 @@ export default {
     triggerButtonId: {
       get () {
         return this.id + '-add-music-modal-trigger'
+      }
+    },
+    fileElementId: {
+      get () {
+        return this.id + '-upload-music-file'
       }
     },
     modalId: {
