30a5755ecd955ff0d95104beb1a8c244379ec714
mansa <deepak057@yahoo.com>
2019-12-05 21:21:27 +0530

    working on background music editing

--- src/components/videoEditing/add_music.vue
+++ src/components/videoEditing/add_music.vue
@@ -38,7 +38,7 @@ div(v-if="triggered")
                   | {{getFileUploadProgressText()}}
                   i.fa.fa-trash.m-l-5.pointer(@click="resetFileUpload()" v-if="track.path" title="Remove this file")
                 </template>
-                input.none(type="file" :id="fileElementId" @change="filesChange($event.target.name, $event.target.files)" onclick="this.value=null;")
+                input.none(:accept="getAcceptedAudioString()" type="file" data-type="audio" :id="fileElementId" @change="filesChange($event.target.name, $event.target.files)")
           .m-b-20.text-center(v-if="trackAdded")
             .alert.alert-success
               | Music Track Uploaded Successfully
