1004fa9fdd7bbfc209a46fbdb4ecb535aa5e1238
mansa <deepak057@yahoo.com>
2021-02-10 23:30:46 +0530

    implementing more background stars in overlay view

--- src/features/dashboard/overlay-view.vue
+++ src/features/dashboard/overlay-view.vue
@@ -4,9 +4,52 @@ div(v-if="triggered")
   .modal.modal-append-to-body.video-overlay-view(:id="modalId" role='dialog', aria-label.smallledby='AdStatsModallabel.small', aria-hidden='true')
     .modal-dialog(v-touch:swipe = "onSwipe")
       .modal-content
+        <template v-if="animationTemplate === 1">
+          -for (i = 1; i <= 100; i++)
+            .circle-container
+              .circle
+        </template>
+        <template v-if="animationTemplate === 2">
         #background
         #midground
         #foreground
+        </template>
+        <template v-if="animationTemplate === 3">
+        #stars
+        #stars2
+        #stars3
+        </template>
+        <template v-if="animationTemplate === 4">
+        .stars
+        .twinkling
+        .clouds
+        </template>
+        <template v-if="animationTemplate === 5">
+        .night
+          - for (i = 0; i < 20; i++)
+            .shooting_star
+        </template>
+        <template v-if="animationTemplate === 6">
+         .background
+          - for (i = 0; i < 20; i++)
+            span
+        </template>
+        <template v-if="animationTemplate === 7">
+          div
+            .light.x1
+            .light.x2
+            .light.x3
+            .light.x4
+            .light.x5
+            .light.x6
+            .light.x7
+            .light.x8
+            .light.x9
+        </template>
+        <template v-if="animationTemplate === 8">
+          - for (var i = 1; i <= 100; i++)
+            .firefly
+        </template>
         .modal-header.no-border(v-if="!isMobile()")
           .row.w-100
             .col-2
@@ -99,7 +142,8 @@ export default {
       nextCommandInvoked: false,
       prevCommandInvoked: false,
       spinRefreshIcon: false,
-      TextNoMoreFeed: 'Sorry, no more posts.'
+      TextNoMoreFeed: 'Sorry, no more posts.',
+      animationTemplate: 8
     }
   },
   computed: {
